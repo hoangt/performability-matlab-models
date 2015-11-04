0203f4b9c96ef68853ed34f30c15b58e5cdadf04
Mark Gottscho <mgottscho@ucla.edu>
2015-11-03 20:01:57 -0800

    partial update

--- performability_model.m
+++ performability_model.m
@@ -1,23 +1,38 @@
-function [p, t_event_faults, percent_slowdown, t_lat_avg, percent_slowdown_avg] = performability_model(k, my_tau, my_gamma, my_lambda, faults_during_fault_handler)
+function [p, t_lats, percent_slowdown, t_lat_avg, percent_slowdown_avg] = performability_model(max_k, taus, my_gamma, my_lambda, faults_during_fault_handler)
 
-t_event_faults = NaN(size(k));
-for i=1:size(k,1)
-    t_event_faults(i) = my_gamma + sum(my_tau(1:i));
+% Compute the event handler latency for each value of K in 1:max_k
+t_lats = NaN(size(max_k));
+for i=1:size(max_k)
+    t_lats(i) = my_gamma + sum(taus(1:i));
 end
+
+% Compute beta_i for i in 1:max_k+1
+betas = NaN(size(max_k)+1);
+betas(1) = my_lambda * my_gamma; % Special case for beta_0
 
 if (faults_during_fault_handler == 1)
-    my_beta = my_lambda.*t_event_faults;
+    betas(2:max_k) = my_lambda * taus(1:max_k);
 else
-    my_beta = my_lambda*my_gamma;
+    betas(2:max_k) = 0;
+end 
+
+% Compute PK for K in 0:max_k
+PK = NaN(size(max_k)+1);
+for k = 0:max_k
+    prefix = exp(-sum(betas(1:k+1)));
+    for j = 0:k-1
+       upper = k-j-sum(
+       for i_j = 0:k-j-su 
+    end
 end
 
-p = my_beta.^k.*exp(-my_beta)./factorial(k);
+p = betas.^max_k.*exp(-betas)./factorial(max_k);
 p(isnan(p))=0;
 
-abs_slowdown = t_event_faults - my_gamma;
+abs_slowdown = t_lats - my_gamma;
 percent_slowdown = abs_slowdown ./ my_gamma * 100;
 
-t_lat_avg = sum(p .* t_event_faults);
+t_lat_avg = sum(p .* t_lats);
 percent_slowdown_avg = (t_lat_avg - my_gamma)/my_gamma * 100;
     
 % if (sum(my_lambda .* my_tau >= 1) < 1)
