a0b12f67c7a4bb8282f742e690a042c326be4014
Mark Gottscho <mgottscho@ucla.edu>
2015-11-03 23:56:11 -0800

    Updated performability model to match what is in Nov 3 version of paper model. There are problems with numeric stability for large betas, or betas that do not converge to 0 as n goes to infinity. Also, the recursive implementation is exponentially slow -- need to use some good old dynamic programming to get past k=10 or so. That should bring the runtime down to O(N squared) or maybe N cubed.

---
+++ f.m
@@ -0,0 +1,30 @@
+function [ret] = f(x,y,betas,init)
+
+persistent ncalls;
+
+if init == 1
+    ncalls = 1
+else
+    ncalls = ncalls + 1
+end
+
+if x >= 0 % normal condition
+    terms = NaN(y+1,1);
+    if betas(x+1) > 1e-12; % Check for beta close to 0: if it is, then don't bother recursing
+        for i=0:y
+            terms(i+1) = betas(x+1)^i / factorial(i) * f(x-1, y+1-i, betas, 0);
+        end
+        ret = sum(terms);
+    else
+        ret = 0;
+    end    
+else
+    if y <= -x % base case: x < 0 and y <= -x
+        ret = 1;
+    else % base case: x < 0 and y > -x
+        ret = 0;
+    end
+end
+
+end
+
