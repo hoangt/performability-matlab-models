c8d664f702c41ff52d7675946a4c926974ed3ea3
Mark Gottscho <mgottscho@ucla.edu>
2015-11-04 18:54:18 -0800

    Some updates and implemented dynamic programming model, but it is currently wrong

--- f.m
+++ f.m
@@ -1,18 +1,10 @@
-function [ret] = f(x,y,betas,init)
-
-persistent ncalls;
-
-if init == 1
-    ncalls = 1
-else
-    ncalls = ncalls + 1
-end
+function [ret] = f(x,y,betas)
 
 if x >= 0 % normal condition
     terms = NaN(y+1,1);
     if betas(x+1) > 1e-12; % Check for beta close to 0: if it is, then don't bother recursing
         for i=0:y
-            terms(i+1) = betas(x+1)^i / factorial(i) * f(x-1, y+1-i, betas, 0);
+            terms(i+1) = betas(x+1)^i / factorial(i) * f(x-1, y+1-i, betas);
         end
         ret = sum(terms);
     else
--- performability_model.m
+++ performability_model.m
@@ -11,16 +11,17 @@ betas = NaN(max_k+1,1);
 betas(1) = my_lambda * my_gamma; % Special case for beta_0
 PK = NaN(max_k+1,1);
 
+%% Recursive implementation
 %if (faults_during_fault_handler == 1)
     betas(2:max_k+1) = my_lambda * taus(1:max_k);
     
     % Compute PK for K in k = 0:max_k.
     % We utilize a max_k*max_k matrix to store incremental results and employ
     % dynamic programming to solve the PK recursive formulation.
-    for k = 0:max_k
-        prefix = exp(-sum(betas(1:k+1)));
-        PK(k+1) = prefix * f(k,0,betas,k==0);
-    end
+  %  for k = 0:max_k
+ %       prefix = exp(-sum(betas(1:k+1)));
+ %       PK(k+1) = prefix * f(k,0,betas);
+ %   end
 %else % Special case
 %    betas(2:max_k+1) = 0;
 %    for k = 0:max_k
@@ -28,6 +29,39 @@ PK = NaN(max_k+1,1);
 %    end
 %end 
 
+%% Dynamic programming implementation
+f_matrix = NaN(max_k+1, max_k+1);
+for x=0:max_k
+    for y=0:max_k-x
+        % Compute f(x,y) using dynamic programming
+        if betas(x+1) > 1e-12; % Check for beta close to 0: if it is, then don't bother computing
+            terms = NaN(y+1,1);
+            for i=0:y
+                tmp1 = betas(x+1)^i / factorial(i);
+                if x-1 < 0 % base cases
+                    if y+1-i <= -(x-1)
+                        tmp2 = 1;
+                    else % y+1-i > -(x-1)
+                        tmp2 = 0;
+                    end
+                else % common case
+                    tmp2 = f_matrix((x-1)+1, (y+1-i)+1);
+                end
+                terms(i+1) = tmp1 + tmp2;
+            end
+            f_matrix(x+1,y+1) = sum(terms);
+        else
+            f_matrix(x+1,y+1) = 0;
+        end
+    end
+end
+
+f_matrix
+
+for k = 0:max_k
+    prefix = exp(-sum(betas(1:k+1)))
+    PK(k+1) = prefix * f_matrix(k+1,1)
+end
 
 
 %p = betas.^max_k.*exp(-betas)./factorial(max_k);
